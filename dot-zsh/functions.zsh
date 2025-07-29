a3e0c259f5afd737ba9a86af3c7b8399e005697e
Michail Bastakis <mbastakis@gmail.com>
2025-07-29 14:13:16 +0300

    fix: claude context7 mcp, added ftext

--- dot-zsh/functions.zsh
+++ dot-zsh/functions.zsh
@@ -63,6 +63,16 @@ function y() {
 	rm -f -- "$tmp"
 }
 
+ftext() {
+  rg --color=always --line-number --no-heading --smart-case "${*:-}" |
+    fzf --ansi \
+        --color "hl:-1:underline,hl+:-1:underline:reverse" \
+        --delimiter : \
+        --preview 'bat --color=always {1} --highlight-line {2}' \
+        --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+        --bind 'enter:execute(code {})';
+}
+
 # Custom functions
 check_repos_behind() {
     echo "Checking for repositories that are behind their remotes..."
