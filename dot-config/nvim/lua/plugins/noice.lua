bc28833bf363427963460f624f6b74fca9fc483f
Michail Bastakis <mbastakis@gmail.com>
2025-12-02 06:49:34 +0200

    fix: noice improvements

--- dot-config/nvim/lua/plugins/noice.lua
+++ dot-config/nvim/lua/plugins/noice.lua
@@ -4,9 +4,8 @@ return {
     "rcarriga/nvim-notify",
     config = function()
       require("notify").setup({
-        -- Notifications come from bottom-right corner
-        stages = "slide", -- Animation style
-        render = "default", -- Render style
+        stages = "slide", -- Animation style: fade_in_slide_out, fade, slide, static
+        render = "default", -- Render style: default, minimal, simple, compact, wrapped-compact
         timeout = 3000, -- Default timeout in ms
         top_down = false, -- Notifications stack from bottom to top
         max_width = 50,
@@ -35,6 +34,54 @@ return {
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
           },
+          -- LSP hover and signature help with compact style
+          hover = {
+            enabled = true,
+            silent = false,
+          },
+          signature = {
+            enabled = true,
+            auto_open = {
+              enabled = true,
+              trigger = true,
+              throttle = 50,
+            },
+          },
+          -- Customize documentation (hover/signature) window
+          documentation = {
+            view = "hover",
+            opts = {
+              lang = "markdown",
+              replace = true,
+              render = "plain",
+              format = { "{message}" },
+              win_options = {
+                concealcursor = "n",
+                conceallevel = 3,
+              },
+            },
+          },
+        },
+        -- Customize views for compact borders
+        views = {
+          hover = {
+            border = {
+              style = "rounded",
+              padding = { 0, 1 }, -- Reduced padding for smaller border
+            },
+            position = {
+              row = 2,
+              col = 2,
+            },
+            size = {
+              max_width = 80,
+              max_height = 20,
+            },
+            win_options = {
+              wrap = true,
+              linebreak = true,
+            },
+          },
         },
         -- you can enable a preset for easier configuration
         presets = {
@@ -42,7 +89,7 @@ return {
           command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
           inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true, -- add a border to hover docs and signature help
+          lsp_doc_border = false, -- DISABLE preset border (we customize it above)
         },
       })
     end,
